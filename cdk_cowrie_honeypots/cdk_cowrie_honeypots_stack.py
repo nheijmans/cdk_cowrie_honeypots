701b01116ae88c6478c6c5456e572062387fa233
Niels Heijmans <niels_heijmans@icloud.com>
2020-06-27 20:04:12 +0200

    Create working stack. Needs debugging of ECS cluster to start container successfully

--- cdk_cowrie_honeypots/cdk_cowrie_honeypots_stack.py
+++ cdk_cowrie_honeypots/cdk_cowrie_honeypots_stack.py
@@ -1,4 +1,5 @@
-from aws_cdk import core
+from aws_cdk import (core, aws_ec2 as ec2, aws_ecs as ecs,
+                     aws_ecs_patterns as ecs_patterns)
 
 
 class CdkCowrieHoneypotsStack(core.Stack):
@@ -6,4 +7,31 @@ class CdkCowrieHoneypotsStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create the VPC for the honeypot(s)
+        vpc = ec2.Vpc(self, "HoneypotVpc", max_azs=3)     # default is all AZs in region
+
+        # Create the ECS cluster where fargate can deploy the Docker containers
+        cluster = ecs.Cluster(self, "HoneypotCluster", vpc=vpc) 
+
+        # Define task definition for Fargate Service
+        task_definition = ecs.FargateTaskDefinition(self, "HoneypotTasks", cpu=256, memory_limit_mib=512)
+
+        # Container definition
+        
+        container_definition = ecs.ContainerDefinition(self, "HoneypotContainerDefinition",
+            image=ecs.ContainerImage.from_registry("statixs/cowrie"), 
+            task_definition=task_definition)
+
+        #container_definition.container_port=22
+        #container_definition.port_mappings=container_definition.find_port_mapping(container_port=2222)
+
+        # ECS Security Group definition
+        sg_ssh = ec2.SecurityGroup(self, "honeypot-sg-ssh", vpc=vpc, description="Allow SSH to the honeypot")
+        sg_ssh.add_ingress_rule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(22))
+
+        # Fargate service definition
+        fargate_service = ecs.FargateService(self, "HoneypotFargate", cluster=cluster, 
+            assign_public_ip=True, 
+            desired_count=2, 
+            security_group=sg_ssh, 
+            task_definition=task_definition)
